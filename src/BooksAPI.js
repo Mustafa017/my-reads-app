f07a2e072fc8b2243e4b2bd7e8f63267a345959d
Mustafa Khamisi <mustafangure95@gmail.com>
2022-05-04 14:53:34 -0500

    fix: Implement error handling using catch statement

--- src/BooksAPI.js
+++ src/BooksAPI.js
@@ -36,7 +36,8 @@ export const search = (query) =>
       ...headers,
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ query }),
+    body: JSON.stringify(query),
   })
     .then((res) => res.json())
-    .then((data) => data.books);
+    .then((data) => data.books)
+    .catch((err) => console.log(err));
